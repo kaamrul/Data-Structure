150ccc7d748ecb6f1c89ba2a66f2f5d731ca9ba4
kaamrul <mk.kamrulhasan@outlook.com>
2019-11-29 00:36:39 +0600

    Add Bubble Sort in C

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,20 +1,18 @@
+// Simple Bubble Sort Algorithm
 #include<stdio.h>
 int main()
 {
     int array[] = {15,16,6,8,5};
     int i, j, temp=0, flag;
     for(i=0; i<5-1; i++){
-        flag = 0;
         for(j=0; j<5-1-i; j++){
             if(array[j] > array[j+1]){
+                // swap the elements
                 temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
-                flag = 1;
             }
         }
-        if(flag == 0)
-        break;
     }
     printf("Sorted Array List: ");
     for(i=0; i<5; i++){
